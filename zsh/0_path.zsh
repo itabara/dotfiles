6f543a773521aee835802b67c7394bca568f48a7
Iulian Tabara <itabara@live.com>
2020-12-25 17:09:06 +0200

    Added pyenv path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -7,8 +7,21 @@ pathAppend() {
   fi
 }
 
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+  autoload -Uz compinit
+  compinit
+fi
+
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
 # Remove duplicate entries from PATH:
 PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
 
 pathAppend "$HOME/.yadr/bin"
 pathAppend "$HOME/.yadr/bin/yadr"
+pathAppend "$PYENV_ROOT/bin"
